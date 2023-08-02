750ce36b6f3c00fc538d43663467e69f29f62561
Julie "Jous" Paillusseau <jous@akiros.io>
2023-08-02 10:43:14 +0200

    Fix readme and add outputs description

--- README.md
+++ README.md
@@ -26,9 +26,10 @@ module "vnet" {
 Defining subnets to deploy within the Vnet (3 here) :
 
 ```hcl
-module "vpc" {
-  source       = "akirosit/vpc-sample/aws"
-  vpc_cidr     = "10.1.0.0/16"
-  subnet_cidrs = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
+module "vnet" {
+  source         = "akirosit/vnet-sample/azurerm"
+  resource_group = "MyRG"
+  vnet_cidr      = ["10.1.0.0/16"]
+  subnet_cidrs   = ["10.1.0.0/24", "10.1.1.0/24", "10.1.2.0/24"]
 }
 ```
--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,29 @@
 output "vnet_id" {
-  value = azurerm_virtual_network.main.id
+  value       = azurerm_virtual_network.main.id
+  description = "The ID of the virtual network"
 }
 
 output "vnet_name" {
-  value = azurerm_virtual_network.main.name
+  value       = azurerm_virtual_network.main.name
+  description = "The name of the virtual network"
 }
 
 output "vnet_address_space" {
-  value = azurerm_virtual_network.main.address_space
+  value       = azurerm_virtual_network.main.address_space
+  description = "The address space of the virtual network"
 }
 
 output "subnet_ids" {
-  value = [for s in azurerm_subnet.main : s.id]
+  value       = [for s in azurerm_subnet.main : s.id]
+  description = "The list of subnet IDs"
 }
 
 output "subnet_names" {
-  value = [for s in azurerm_subnet.main : s.name]
+  value       = [for s in azurerm_subnet.main : s.name]
+  description = "The list of subnet names"
 }
 
-output "subnet_address_spaces" {
-  value = [for s in azurerm_subnet.main : s.address_prefixes]
+output "subnet_address_prefixes" {
+  value       = [for s in azurerm_subnet.main : s.address_prefixes]
+  description = "The list of subnet address prefixes"
 }
